d6efb660c9306696076cd8f4d1078467bb73aece
zafaremon20 <zafar emon>
2023-03-19 23:24:27 +0600

    implement headless ui components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyDropdown from "./components/MyDropdown";
+import MyListBox from "./components/MyListBox";
+import MyComboBox from "./components/MyComboBox";
+import MyToggle from "./components/MyToggle";
+import MyFAQ from "./components/MyFAQ";
+import MyPOpover from "./components/MyPopover";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-800 min-h-screen">
+      <MyDropdown />
+      <MyListBox />
+      <MyComboBox />
+      <MyToggle />
+      <MyFAQ />
+      <MyPOpover />
     </div>
   );
 }
